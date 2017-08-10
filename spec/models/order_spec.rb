3014660004216c153b1d4c39498a6aabbb1bbe8c
alexandre-h <herole.alexandre@gmail.com>
2017-08-10 20:03:25 +0200

    delete comment

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Order, type: :model do
   it { should belong_to(:customer) }
   it { should belong_to(:rider) }
   it { should belong_to(:restaurant) }
-  #it { should validate_presence_of(:x) }
-  #it { should validate_presence_of(:y) }
-  #it { should have_one(:card) }
-  #it { should have_many(:decks) }
 end
